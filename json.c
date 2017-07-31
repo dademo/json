2b986322229d6402492f6742e41c590e982049d6
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-07-31 18:54:42 +0200

    Making the function to create a jsonObject

--- json.c
+++ json.c
@@ -8,18 +8,18 @@
 #include "myCRC.h"
 
 int scanForArrays(char* data);
-int getObjectDataSize(char* data);
+int getObjectDataSize(char* data);	// Include '{' and '}'
 
 struct jsonObject* mkObject(char* jsonReceivedData);
 struct jsonArrayElem* mkArray(char* jsonReceivedData);
-void jsonObject_addData(struct jsonObject* obj, char* name, jsonObjectType type, char* data);
+void jsonObject_addData(struct jsonObject* obj, char* name, enum jsonObjectType type, char* data);
 
 
 struct json* mkJsonData(char* jsonReceivedData)
 {
 	printf("Found %d arrays\n", scanForArrays(jsonReceivedData));
 	printf("Main object is %d size\n", getObjectDataSize(jsonReceivedData));
-	printf("Second object is %d size\n", getObjectDataSize(jsonReceivedData+20));
+	printf("Second object is %d size\n", getObjectDataSize(jsonReceivedData+10));
 	printf(" : %s", jsonReceivedData+20);
 
 	struct json* toReturn = (struct json*) malloc(sizeof(struct json));
@@ -27,7 +27,7 @@ struct json* mkJsonData(char* jsonReceivedData)
 	unsigned int i = 0;
 
 	while(jsonReceivedData[i] != '{'){ i++; }
-	toReturn->mainObject = mkObject(jsonReceivedData+i);
+	//toReturn->mainObject = mkObject(jsonReceivedData+i);
 	
 	return 0;
 }
@@ -36,24 +36,57 @@ struct jsonObject* mkObject(char* jsonReceivedData)
 {
 	printf("jsonObject:\n%s\n", jsonReceivedData);
 	
-	int i = 0;
-	int objectLevel = 0;	// Root level == 0
+	int i = 0, j = 0;
+	char tmpName[100] = "";	// To store name temporarily
+	char tmpData[500] = "";	// To store data temporarily
+	enum jsonObjectType tmpType = jsondata_unknown;
 	struct jsonObject* currentObject = (struct jsonObject*) malloc(sizeof(struct jsonObject));
+
+	while(jsonReceivedData[i] != '\0' && jsonReceivedData[i] != '{') { i++; }
 
 	while(jsonReceivedData[i] != '\0')
 	{
-		switch(jsonReceivedData[i]) {
-		case '{':	// Object open
-			if(objectLevel == 0) { objectLevel++; }	// Beginning of the object
-			else { mkObject(jsonReceivedData+i); }
+		switch(jsonReceivedData[i])
+		{
+		case ',':	// end of data, beggining of name
+			while(jsonReceivedData[i] != '"' && jsonReceivedData[i] != '\0'){ i++; }
+			i++;	// We are on the first '"'
+			j = 0;
+			while(jsonReceivedData[i] != '"' && jsonReceivedData[i] != '\0')	// While not reached the end of the name, the second '"'
+			{
+				tmpName[j] = jsonReceivedData[i];
+				i++; j++;
+			}
 			break;
-		case '}':
-			objectLevel--;
-		default:
+		case ':':	// Data
+			while(jsonReceivedData[i] == ' ' && jsonReceivedData[i] != '\0'){ i++; }
+			if(jsonReceivedData[i] == '{')
+			{
+				currentObject->data->objectData = mkObject(jsonReceivedData+i);
+				i += getObjectDataSize(jsonReceivedData+i) - 1;
+			}
+			else
+			{
+				i++;	// To begin the name gestion
+				j = 0;
+				if(jsonReceivedData[i] == '"') { tmpType = jsondata_string; i++; }
+				while(jsonReceivedData[i] != ',' && ((tmpType == jsondata_string)? jsonReceivedData[i] != '"' : 1 ) )
+				{
+					tmpData[j] = jsonReceivedData[i];
+				}
+				if(tmpType == jsondata_string){ i++; };	// We ignore the second '"' too
+				// Creating the current data
+				jsonObject_addData(currentObject, tmpName, tmpType, tmpData);
+				// Reset data
+				tmpData[0] = '\0';
+				tmpName[0] = '\0';
+				tmpType = jsondata_unknown;
+			}
+			
 			break;
 		}
+		i++;
 	}
-
 	return 0;
 }
 
@@ -65,7 +98,7 @@ struct jsonArrayElem* mkArray(char* jsonReceivedData)
 
 // Object functions
 // Add a "struct jsonData_elem" to a "struct jsonObject" for single data type
-void jsonObject_addData(struct jsonObject* obj, char* name, jsonObjectType type, char* data)
+void jsonObject_addData(struct jsonObject* obj, char* name, enum jsonObjectType type, char* data)
 {
 	struct jsonData_elem* dataToAdd = (struct jsonData_elem*) malloc(sizeof(struct jsonData_elem));	// Allocating memory for the jsonData_elem structure
 	// Setting values for the dataToAdd structure --> Single object //
@@ -125,7 +158,7 @@ int getObjectDataSize(char* data)
 		if(data[i] == '{'){ dataObjectLevel++; }
 		if(data[i] == '}')
 		{
-			if(dataObjectLevel == 1){ return (int) (i - initPos); }
+			if(dataObjectLevel == 1){ return (int) (i - initPos - 1); }
 			dataObjectLevel--;
 		}
 	}
