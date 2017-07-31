bf242a380dc1777e4bdf5a6f73db176e72ea6e87
Damien DEMONAZ <damien.demonaz@gmail.com>
2017-07-31 16:27:22 +0200

    modifié :         json.h 	--> Modified json structure

--- json.h
+++ json.h
@@ -3,35 +3,66 @@
 
 #include <stdint.h>	// int64_t
 
+// Type of the JSON data, for the information
 enum jsonObjectType {
-	jsondata_string,
-	jsondata_null,
-	jsondata_bool,
+	jsondata_string,	// char*
+	jsondata_null,		// == 0
+	jsondata_bool,		// boolean (int : 0<>1)
 	jsondata_array,		// array of jsonData
-	jsondata_nbr,		// double
+	jsondata_nbr,		// double (char*)
+	jsondata_object,	// struct jsonObject
 	jsondata_unknown	// default, if doesn't know
 };
 
-struct jsonData {
-	struct jsonData_elem* first;
+// JSON data structure
+struct json {
+	struct jsonObject* mainObject;	// Main object of the JSON structure
 };
 
-struct jsonData_elem {
-	char* name;			// for root data, name = root
-	int64_t namecrc;		// custom checksum, for fast data get
-	enum jsonObjectType datatype;	// Type of data, no use but for the information
-	char* data;			// data if not an array
-	int countArray;			// if array, size of the array
-	struct jsonData* arrayData;	// If an array, sore data here
-	struct jsonData_elem* next;		// if == 0, no element after
-};
-
+// JSON object structure
 struct jsonObject {
 	struct jsonData* data;	// Chained list of struct jsonData
 };
 
-struct jsonObject* mkJsonData(char* jsonReceivedData);
+// JSON array structure (dynamic array of jsonObject*)
+struct jsonArrayElem {
+	struct jsonObject* elem;	// data
+	struct jsonArrayElem* next;	// if == 0, no element after
+};
+
+// JSON data itself
+struct jsonData_elem {
+	char* name;			// name of the variable
+	int64_t namecrc;		// custom checksum, for fast data get
+	enum jsonObjectType datatype;	// type of data, no use but for the information
+	char* data;			// data if not an array
+	struct jsonArrayElem* arrayData;// if an array, pointer to the first element
+	struct jsonObject* objectData;	// if data is as object, pointer to the object
+	struct jsonData_elem* next;	// if == 0, no element after
+};
+
+/*
+ *	struct json
+ *	     |--> struct jsonObject
+ *			|--> struct jsonData_elem (char* name)
+ *				   |--> char* data
+ *			    (OR)   |--> struct jsonArrayElem
+ *						|--> struct jsonObject -> (...)
+ *					      (...)
+ *				   |............|
+ *				   |
+ *			    (OR)   |--> struct jsonObject
+ *						|
+ *					      (...)
+ *				   |............|
+ *			|..........|
+ *	    |...........|
+ *				
+ */
+
+
+struct json* mkJsonData(char* jsonReceivedData);
 void delJsonDataStruct(struct jsonObject* dataroot);
-struct jsonObject* getJsonData(char* path);	// Dotted path (ex: 0.Data.x)
+struct json* getJsonData(char* path);	// Dotted path (ex: 0.Data.x)
 
 #endif
